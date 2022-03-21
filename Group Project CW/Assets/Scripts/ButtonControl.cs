3d9f25abce6a3aa7fd7b793752c09db4eba88eae
Rohit7S <ms1318e@gre.ac.uk>
2022-03-21 16:58:12 +0000

    Made Loading screen and added title in main menu

--- Group Project CW/Assets/Scripts/ButtonControl.cs
+++ Group Project CW/Assets/Scripts/ButtonControl.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class ButtonControl : MonoBehaviour
 {
-    public void Play()
+    AsyncOperation loadingOperation;
+
+    public GameObject MenuScreen;
+    public Slider progressSlider;
+    public GameObject SliderGO;
+    public Text progress;
+    public void Play(string scenename)
     {
-        string SceneName = "";
-        SceneManager.LoadScene("GameScene");
+        MenuScreen.SetActive(false);
+        SliderGO.SetActive(true);
+        StartCoroutine(Loading(scenename));
     }
     public void Exit()
     {
         Application.Quit();
     }
-
     public void Controls()
     {
         SceneManager.LoadScene("Controls");
@@ -23,5 +29,18 @@ public class ButtonControl : MonoBehaviour
     public void Menu()
     {
         SceneManager.LoadScene("Start");
+    }
+    IEnumerator Loading(string scenename)
+    {
+        loadingOperation = SceneManager.LoadSceneAsync(scenename);
+
+        while (!loadingOperation.isDone)
+        {
+            float progressPercent = Mathf.Clamp01(loadingOperation.progress / 0.9f);
+            progressSlider.value = progressPercent;
+            progress.text = progressPercent * 100 + "%";
+
+            yield return null; 
+        }
     }
 }
